c53bb1a51402f32f52d54704c4d143c60e5ddadb
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-06-21 12:49:21 -0400

    Add labels to vertexes

--- src/draw.py
+++ src/draw.py
@@ -2,8 +2,15 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
 from bokeh.palettes import Spectral8
+from bokeh.models import (
+    GraphRenderer,
+    StaticLayoutProvider,
+    Oval,
+    ColumnDataSource,
+    LabelSet,
+    Label,
+)
 
 from graph import *
 
@@ -33,14 +40,15 @@ graph.node_renderer.glyph = Oval(height=30, width=30, fill_color="color")
 
 # this is drawing the edges from start to end
 graph.edge_renderer.data_source.data = dict(start=[], end=[])
+
 for v in graph_data.vertexes:
     if len(v.edges) > 0:
         for e in v.edges:
             self_index = graph_data.vertexes.index(v)
-            graph.edge_renderer.data_source.data['start'].append(self_index)
+            graph.edge_renderer.data_source.data["start"].append(self_index)
 
             dest_index = graph_data.vertexes.index(e.destination)
-            graph.edge_renderer.data_source.data['end'].append(dest_index)
+            graph.edge_renderer.data_source.data["end"].append(dest_index)
 
 
 # start of layout code
@@ -48,11 +56,25 @@ for v in graph_data.vertexes:
 x = [v.pos["x"] for v in graph_data.vertexes]
 y = [v.pos["y"] for v in graph_data.vertexes]
 
+# draw labels
+label_data = dict(x=x, y=y, names=[v.value for v in graph_data.vertexes])
+source = ColumnDataSource(data=label_data)
+labels = LabelSet(
+    x="x",
+    y="y",
+    x_offset=-7,
+    y_offset=-40,
+    text="names",
+    level="glyph",
+    source=source,
+    render_mode="canvas",
+)
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
+plot.renderers.append(labels)
 
 output_file("graph.html")
 show(plot)
